933e948f6e7b3991da13164f6a4570b93b1c8500
Praveenajayathissa <as20240518@sci.sjp.ac.lk>
2025-10-26 18:11:23 +0530

    Add main menu

--- main.c
+++ main.c
@@ -1,3 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_CITIES 30
+#define NAME_LENGTH 50
+
+int main() {
+    char cityList[MAX_CITIES][NAME_LENGTH];
+    int distanceMap[MAX_CITIES][MAX_CITIES] = {0};
+    int totalCities = 0;
+    int menuOption;
+
+    do {
+        printf("\n===== Logistics Control System =====\n\n");
+        printf("1. Manage Cities\n");
+        printf("2. Manage Distances\n");
+        printf("3. Vehicle Information\n");
+        printf("4. Delivery Handling\n");
+        printf("5. Reports Section\n");
+        printf("6. Quit Program\n");
+        printf("-----------------------------------\n");
+        printf("Select an option: ");
+        scanf("%d", &menuOption);
+        getchar();
+
+        switch (menuOption) {
+        case 1:
+
+            break;
+
+        case 2:
+
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        case 5:
+
+            break;
+
+        case 6:
+            printf("Thank you for using the system. Goodbye!\n");
+            break;
+
+        default:
+            printf("Invalid selection. Please try again.\n");
+            break;
+        }
+
+    } while (menuOption != 6);
+
+    return 0;
+}
 
